28dbbbd9764c4229f85758d0293d266adbda00ee
lucas fernández <lfaragon@visiotech.es>
2017-01-23 22:51:28 +0100

    Initial Commit

---
+++ AffinCipher/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AffinCipher
+//
+//  Created by lucas fernández on 23/01/2017.
+//  Copyright © 2017 lucas fernández. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
